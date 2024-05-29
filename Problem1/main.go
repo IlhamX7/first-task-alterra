69b695c5856ba5697560875df8a37397e0cf7f1e
IlhamX7 <ilham.ilham1997@yahoo.co.id>
2024-05-29 21:47:50 +0700

    add task pointer problem 1 and 2

--- Problem1/main.go
+++ Problem1/main.go
@@ -4,32 +4,16 @@ import (
 	"fmt"
 )
 
-func Compare(a, b string) string {
-	m, n := len(a), len(b)
-	longest, endIndex := 0, 0
-
-	lcsuff := make([][]int, m+1)
-	for i := range lcsuff {
-		lcsuff[i] = make([]int, n+1)
-	}
-
-	for i := 1; i <= m; i++ {
-		for j := 1; j <= n; j++ {
-			if a[i-1] == b[j-1] {
-				lcsuff[i][j] = lcsuff[i-1][j-1] + 1
-				if lcsuff[i][j] > longest {
-					longest = lcsuff[i][j]
-					endIndex = i
-				}
-			}
-		}
-	}
-
-	return a[endIndex-longest : endIndex]
+func swap(a, b *int) {
+	temp := *a
+	*a = *b
+	*b = temp
 }
 
 func main() {
-	a := "KUPU-KUPU"
-	b := "KUPU"
-	fmt.Println(Compare(a, b))
+	a := 10
+	b := 20
+
+	swap(&a, &b)
+	fmt.Println(a, b)
 }
--- Problem2/main.go
+++ Problem2/main.go
@@ -2,24 +2,33 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
-func caesar(offset int, input string) string {
-	shifted := []rune(input)
-
-	for i, char := range input {
-		if char >= 'a' && char <= 'z' {
-			shifted[i] = 'a' + (char-'a'+rune(offset))%26
-		} else {
-			shifted[i] = char
+func getMinMax(numbers ...*int) (int, int) {
+	max := math.MinInt
+	min := math.MaxInt
+	for _, num := range numbers {
+		if *num > max {
+			max = *num
+		}
+		if *num < min {
+			min = *num
 		}
 	}
-
-	return string(shifted)
+	return max, min
 }
 
 func main() {
-	offset := 2
-	input := "alta"
-	fmt.Println(caesar(offset, input))
+	var a1, a2, a3, a4, a5, a6, min, max int
+	fmt.Println("Masukkan 6 angka:")
+	fmt.Scanln(&a1)
+	fmt.Scanln(&a2)
+	fmt.Scanln(&a3)
+	fmt.Scanln(&a4)
+	fmt.Scanln(&a5)
+	fmt.Scanln(&a6)
+	max, min = getMinMax(&a1, &a2, &a3, &a4, &a5, &a6)
+	fmt.Println("Nilai min ", min)
+	fmt.Println("Nilai max ", max)
 }
